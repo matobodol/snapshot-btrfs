d9b24d667514f5b8500de802172a516b3fa049bc
matobodol <matobodol@gmail.com>
2023-05-30 04:24:18 +0700

    up

--- menu.sh
+++ menu.sh
@@ -5,18 +5,23 @@ clear
 # TARGET : value (root/home)
 # TARGET_PATH : value (/dev/sdxY)
 
+target_path(){
+	if [ "$TARGET" == 'root' ]; then
+		TARGET_PATH=$(lsblk -o path,mountpoint | awk '$2=="/" {print $1}')
+		MOUNTPOINT='/'
+	elif [ "$TARGET" == 'home' ]; then
+		TARGET_PATH=$(lsblk -o path,mountpoint | awk '$2=="/home" {print $1}')
+		MOUNTPOINT='/home'
+	fi	
+}
+
 umount_target_path() {
 	local MNT=$(lsblk -o path,mountpoint | awk '$2=="/mnt" {print $1}')
 	[ -n "$MNT" ] && sudo umount /mnt && echo "umount $MNT from /mnt sukses"
 }
 
 mount_target_path(){
-	if [ "$TARGET" == 'root' ]; then
-		TARGET_PATH=$(lsblk -o path,mountpoint | awk '$2=="/" {print $1}')
-	elif [ "$TARGET" == 'home' ]; then
-		TARGET_PATH=$(lsblk -o path,mountpoint | awk '$2=="/home" {print $1}')
-	fi
-	
+	target_path	
 	[ -n "$TARGET_PATH" ] && sudo mount -t btrfs -o subvolid=5 $TARGET_PATH /mnt && echo "mount $TARGET_PATH to /mnt sukses"
 }
 
@@ -126,6 +131,69 @@ restore_delete(){
 	fi
 }	
 
+check_active_snapshot(){
+	local DEFAULT_NAME UUID_TARGET_PATH ACTIVE_SNAPSHOT GEN_MAX PATH_FSTAB
+	DEFAULT_NAME=@active_home
+	PATH_FSTAB=tes.txt
+	
+	target_path
+	
+	# Mendapatkan nilai gen tertinggi pada daftar snapshot yg ada
+	GEN_MAX=$(sudo btrfs subvolume list $MOUNTPOINT | awk '/gen/ { if ($4 > max) max = $4 } END { print max }')
+	
+	if [ -n "GEN_MAX" ]; then
+		
+		# mendapatkan nama snapshot yg sedang aktif berdasarkan nilai gen tertinggi
+		ACTIVE_SNAPSHOT=$(sudo btrfs subvolume list $MOUNTPOINT | grep "$GEN_MAX" | awk '{print $9}')
+		
+		# Setup default snapshot
+		if [ -n "$ACTIVE_SNAPSHOT" ] && [ -d "$ACTIVE_SNAPSHOT" ]; then
+		
+			#~ mv /mnt/$ACTIVE_SNAPSHOT /mnt/$DEFAULT_NAME
+			mv $ACTIVE_SNAPSHOT $DEFAULT_NAME
+			
+		else
+		
+			if [ "$TARGET" == 'root' ]; then
+				#~ [ -n "$NAMESNAPSHOT" ] && sudo btrfs subvolume creat $DEFAULT_NAME
+				#~ sudo btrfs subvolume snapshot $MOUNTPOINT $DEFAULT_NAME
+				touch $DEFAULT_NAME
+				[ "$?" -eq 0 ] && msg="Membuat default subvolume."
+			elif [ "$TARGET" == 'home' ]; then
+				#~ [ -n "$NAMESNAPSHOT" ] && btrfs subvolume creat $DEFAULT_NAME
+				#~ btrfs subvolume snapshot $MOUNTPOINT $DEFAULT_NAME
+				touch $DEFAULT_NAME
+				[ "$?" -eq 0 ] && msg="Membuat default subvolume."
+			fi
+			
+		fi
+		
+		# Setup /etc/fstab
+		if [[ -n $(sed -n '/\/home/p' $PATH_FSTAB) ]]; then
+		
+			# Menambahkan komentar pada baris dengan kata kunci "/home" jika belum ada komentar
+			sed -i '/^\/home/ { /^[^#]/ s/^/#/ }' $PATH_FSTAB
+			[ "$?" -ne 0 ] && echo 'sed -i '/^\/home/ { /^[^#]/ s/^/#/ }' error'
+			
+			# Mendapatkan uuid $TARGET_PATH
+			UUID_TARGET_PATH=$(sudo blkid -s UUID -o value $TARGET_PATH)
+
+			# Menentukan variabel dynamic_content
+			ADD_FSTAB="UUID=$UUID_TARGET_PATH /home btrfs defaults,subvol=$DEFAULT_NAME 0 2"
+
+			# Menambahkan baris dengan konten dinamis pada file "fstab"
+			if [[ -z $(sed -n "/$DEFAULT_NAME/p" $PATH_FSTAB) ]]; then
+				sed -i "\$a$ADD_FSTAB" $PATH_FSTAB
+				[ "$?" -ne 0 ] && echo 'sed -i "\$a$ADD_FSTAB" error'
+			fi
+			# Mengganti nama snapshot pada fstab
+			sed -i "s/$ACTIVE_SNAPSHOT/$DEFAULT_NAME/g" $PATH_FSTAB
+			[ "$?" -ne 0 ] && echo 'sed -i "s/$ACTIVE_SNAPSHOT/$DEFAULT_NAME/g" error'
+		fi
+		
+	fi
+	
+}
 
 main_menu(){
 	local options MENU msg
@@ -138,6 +206,7 @@ main_menu(){
 			'create' ' : buat snapshot baru' 
 			'restore' ' : Pilih snapshot dari daftar lalu restore'
 			'delete' ' : Pilih snapshot dari daftar lalu hapus'
+			'active_snapshot' 'cek active snapshot'
 		)
 		MENU=$(menu_box "$msg" 'MAIN MENU' 'Back' "${options[@]}")
 		[ "$?" -ne 0 ] && break
@@ -151,6 +220,9 @@ main_menu(){
 			;;
 			'delete' )
 				restore_delete 'delete_snapshot'
+			;;
+			'active_snapshot' )
+				check_active_snapshot
 			;;
 		esac
 		ls /mnt
